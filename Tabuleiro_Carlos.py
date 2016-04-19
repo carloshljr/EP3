b5313daf371cbd02dd580d66b36e32a60b515309
carlosludmanjr <carloshlj@al.insper.edu.br>
2016-04-19 17:29:40 -0300

    Tentando fazer o tabuleiro mostrar a proxima jogada = sem sucesso

--- Tabuleiro_Carlos.py
+++ Tabuleiro_Carlos.py
@@ -12,36 +12,32 @@ class Tabuleiro():
     
     def __init__(self,nome):
         self.root = tk.Tk()
-        self.Canvas = tk.Canvas(self.root,width = 200, height = 200)
-        self.nome = self.root.title(nome)
-        def callback1():
-            print(1)  
-        def callback2():
-            print(2) 
-        def callback3():
-            print(3) 
-        def callback4():
-            print(4) 
-        def callback5():
-            print(5) 
-        def callback6():
-            print(6) 
-        def callback7():
-            print(7) 
-        def callback8():
-            print(8) 
-        def callback9():
-            print(9) 
-        self.b1 = tk.Button(self.root, text="OK1", command=callback1, width = 10, height = 5)
-        self.b2 = tk.Button(self.root, text="OK2", command=callback2, width = 10, height = 5)
-        self.b3 = tk.Button(self.root, text="OK3", command=callback3, width = 10, height = 5)        
-        self.b4 = tk.Button(self.root, text="OK4", command=callback4, width = 10, height = 5)
-        self.b5 = tk.Button(self.root, text="OK5", command=callback5, width = 10, height = 5)
-        self.b6 = tk.Button(self.root, text="OK6", command=callback6, width = 10, height = 5)
-        self.b7 = tk.Button(self.root, text="OK7", command=callback7, width = 10, height = 5)
-        self.b8 = tk.Button(self.root, text="OK8", command=callback8, width = 10, height = 5)
-        self.b9 = tk.Button(self.root, text="OK9", command=callback9, width = 10, height = 5)
+        self.label1 = tk.Label()
+        self.label2 =tk.Label()
+        self.label3 = tk.Label()
+        self.label1.grid(row = 0, column = 0)
+        self.label2.grid(row = 1, column = 0)
+        self.label3.grid(row = 2, column = 0)
+        self.Canvas = tk.Canvas(self.label1,width = 200, height = 20)
+        self.Canvas.create_text(80,10,text = "Tranquilidade na nave", fill = "red")
+        self.Canvas.pack()
+        self.Canvas2 = tk.Canvas(self.label3,width = 200, height = 20)
+        def callback1(lista):
+            lista.append(1)
         
+        jogadas = []
+        self.b1 = tk.Button(self.label2, text="OK1", width = 10, height = 5)
+        self.b2 = tk.Button(self.label2, text="OK2",width = 10, height = 5)
+        self.b3 = tk.Button(self.label2, text="OK3", width = 10, height = 5)        
+        self.b4 = tk.Button(self.label2, text="OK4",  width = 10, height = 5)
+        self.b5 = tk.Button(self.label2, text="OK5", width = 10, height = 5)
+        self.b6 = tk.Button(self.label2, text="OK6", width = 10, height = 5)
+        self.b7 = tk.Button(self.label2, text="OK7", width = 10, height = 5)
+        self.b8 = tk.Button(self.label2, text="OK8", width = 10, height = 5)
+        self.b9 = tk.Button(self.label2, text="OK9", width = 10, height = 5)
+        
+        
+        self.b1.configure(command = callback1(jogadas))
         self.b1.pack()
         self.b2.pack()
         self.b3.pack()
@@ -61,9 +57,12 @@ class Tabuleiro():
         self.b7.grid(row = 2, column = 0)
         self.b8.grid(row = 2, column = 1)
         self.b9.grid(row = 2, column = 2)
-    
-      
-    
-         
+        
+        if len(jogadas) % 2 ==0 :
+            self.Canvas2.create_text(80,10,text = "Proxima jogada:X", fill = "red")
+        else:
+            self.Canvas2.create_text(80,10,text = "Proxima jogada:O", fill = "red")
+        self.Canvas2.pack()
+        
 a = Tabuleiro("jogo da velha")
 tk.mainloop()
